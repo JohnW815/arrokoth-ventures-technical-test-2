d77632a8fa5be75146b925d5c7c33ac0160a0258
JohnW815 <83156566+JohnW815@users.noreply.github.com>
2021-09-09 21:45:17 +0800

    Delete arrokoth-ventures-technical-test-1 directory

--- arrokoth-ventures-technical-test-1/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import dataListReducer from './dataListSlice'
-
-export default configureStore({
-    reducer:{
-        dataList: dataListReducer,
-    },
-});
