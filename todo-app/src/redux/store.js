096f07ed3cc117602f0203eb662ea08795dd9425
JohnW815 <83156566+JohnW815@users.noreply.github.com>
2021-09-09 12:59:09 +0800

    Delete todo-app directory

--- todo-app/src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import {createStore,applyMiddleware} from 'redux';
-import { addTodo } from './reducer/addTodo.reducer'
-import logger from 'redux-logger'
-
-export const store = createStore(addTodo,applyMiddleware(logger))
